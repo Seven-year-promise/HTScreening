90b11c659382c9fbaf9c84b2f13d8da5ed0d20cf
yankee <yankee.wann@gmail.com>
2022-04-03 15:23:29 +0200

    visualize wt data 2022 0403

---
+++ Visualization_Statistics/vi_WT_ori_fish.py
@@ -0,0 +1,49 @@
+import os
+import math
+import csv
+import sys
+from skimage import io, draw, color, transform
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_wt_data(path):
+    all_data = {}
+    wt_files = os.listdir(path)
+    for w_f in wt_files:
+        type_name = w_f[0]
+        if not(type_name in all_data):
+            all_data[type_name] = []
+        with open(path+w_f, newline='') as csv_f:
+            read_lines = csv.reader(csv_f, delimiter=",")
+            wt_d_l = []
+            for j, l in enumerate(read_lines):
+                if j > 0:
+                    data_line = [float(i) for i in l]
+                    wt_d_l.append(data_line)
+            wt_d_l = np.array(wt_d_l, np.float)
+            print(wt_d_l.shape)
+
+            for i in range(wt_d_l.shape[1]):
+                # print(list(d_l[:, i]))
+                all_data[type_name].append(list(wt_d_l[:, i]))
+
+    return all_data
+
+def visualize(data_dict={}):
+    wt_types = data_dict.keys()
+    fig, axs = plt.subplots(len(wt_types))
+    for w, wt in enumerate(wt_types):
+        print(w, wt)
+        num = len(data_dict[wt])
+        for w_d in data_dict[wt]:
+            axs[w].plot(w_d)
+        axs[w].set_ylabel("motion index")
+        axs[w].set_xlabel("time")
+        axs[w].set_title(wt + "("+str(num)+")")
+        axs[w].margins(x=0)
+    plt.tight_layout()
+    plt.show()
+if __name__ == "__main__":
+    wt_data = get_wt_data(path="/srv/yanke/PycharmProjects/HTScreening/data/cleaned_data/WT/")
+    visualize(wt_data)
+    #split_data(datas, labels, save_path="./data/effected_dataset/")
